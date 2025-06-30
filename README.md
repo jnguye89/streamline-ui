d4b20e2eec555da50127495d0f5e0f03196fffb9
Jackie Nguyen <jnguye89@gmail.com>
2025-06-29 17:34:03 -0700

    Name change

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Streamline
+# Skriin
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.1.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'streamline' title`, () => {
+  it(`should have the 'skriin' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('streamline');
+    expect(app.title).toEqual('skrii ');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, streamline');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, skriin');
   });
 });
