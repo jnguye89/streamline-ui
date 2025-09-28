5c738108d5baa81be8ac2801ad46343c675157b7
Jackie <jnguye89@gmail.com>
2025-09-28 15:31:39 -0700

    Update auth0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,9 @@ export const environment = {
   streamUrl: "https://3b7d4b5b28f6.us-west-2.playback.live-video.net/api/video/v1/us-west-2.578074109079.channel.kqI34tnoji5s.m3u8",
   auth0: {
     domain: "auth.skriin.com",
-    clientId: "Vlunm6En7MF2N8dzGFOl5sTzcCWX7QRi",
+    clientId: "muw5mgWV9EuZLaRz1yR2S1wGT4I4C2Zm",
     audience:
-      "http://streamlineapiv1-qa.eba-zte7tjpb.us-west-1.elasticbeanstalk.com/",
+      "https://api.skriin.com/",
   },
   ivsIngestEndpoint: 'rtmps://3b7d4b5b28f6.global-contribute.live-video.net:443/app',
   ivsStreamKey: 'sk_us-west-2_o7Qj4w0Hymqs_v7RiHpdGHgb54zph1fjZAd0aDzRICb'
