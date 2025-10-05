b65c2afcf0e3483dd6c4995d8fdf2fd5dfa058a2
Jackie <jnguye89@gmail.com>
2025-10-05 15:23:28 -0700

    clena up calls

--- src/app/components/calls/calls.component.ts
+++ src/app/components/calls/calls.component.ts
@@ -17,12 +17,12 @@ import { SeoService } from "../../services/seo.service";
   styleUrl: "./calls.component.scss",
 })
 export class CallsComponent implements OnInit {
-  targetUser: string = "receiver@app.account.voximplant.com";
+  // targetUser: string = "receiver@app.account.voximplant.com";
   isAuthenticated$: Observable<boolean> = this.auth.isAuthenticated$;
-  @ViewChild("remoteAudio", { static: true })
-  remoteAudioRef!: ElementRef<HTMLAudioElement>;
-  @ViewChild("localAudio", { static: true })
-  localAudioRef!: ElementRef<HTMLAudioElement>;
+  // @ViewChild("remoteAudio", { static: true })
+  // remoteAudioRef!: ElementRef<HTMLAudioElement>;
+  // @ViewChild("localAudio", { static: true })
+  // localAudioRef!: ElementRef<HTMLAudioElement>;
 
   constructor(
     private auth: AuthService,
@@ -33,57 +33,57 @@ export class CallsComponent implements OnInit {
 
   async ngOnInit() {
     this.setUpSeo();
-    combineLatest([this.auth.isAuthenticated$, this.auth.isLoading$])
-      .pipe(
-        filter(([_, isLoading]) => !isLoading),
-        take(1),
-        switchMap(([isAuthenticated]) => {
-          if (!isAuthenticated) {
-            this.auth.loginWithRedirect({
-              appState: {
-                // -> comes back to us after login
-                target: this.router.url,
-              },
-            });
-            // Return an empty observable since we're redirecting
-            return [];
-          } else {
-            // Return the observable for the user integration
-            navigator.mediaDevices
-              .getUserMedia({ audio: true })
-              .then(() => console.log("🎤 Mic access granted"))
-              .catch((err) => console.error("❌ Mic access DENIED", err));
-            return this.getVoxUser();
-          }
-        })
-      )
-      .subscribe((userIntegration: UserIntegration) => {
-        if (userIntegration) {
-          this.voximplantService.login(userIntegration.integrationUsername);
-        }
-      });
+    // combineLatest([this.auth.isAuthenticated$, this.auth.isLoading$])
+    //   .pipe(
+    //     filter(([_, isLoading]) => !isLoading),
+    //     take(1),
+    //     switchMap(([isAuthenticated]) => {
+    //       if (!isAuthenticated) {
+    //         this.auth.loginWithRedirect({
+    //           appState: {
+    //             // -> comes back to us after login
+    //             target: this.router.url,
+    //           },
+    //         });
+    //         // Return an empty observable since we're redirecting
+    //         return [];
+    //       } else {
+    //         // Return the observable for the user integration
+    //         navigator.mediaDevices
+    //           .getUserMedia({ audio: true })
+    //           .then(() => console.log("🎤 Mic access granted"))
+    //           .catch((err) => console.error("❌ Mic access DENIED", err));
+    //         return this.getVoxUser();
+    //       }
+    //     })
+    //   )
+    //   .subscribe((userIntegration: UserIntegration) => {
+    //     if (userIntegration) {
+    //       this.voximplantService.login(userIntegration.integrationUsername);
+    //     }
+    //   });
 
-    this.voximplantService.listen(this.remoteAudioRef);
+    // this.voximplantService.listen(this.remoteAudioRef);
   }
 
   logout() {
     this.auth.logout();
   }
 
-  private getVoxUser(): Observable<UserIntegration> {
-    return this.voximplantService.getVoxImplantUser();
-  }
+  // private getVoxUser(): Observable<UserIntegration> {
+  //   return this.voximplantService.getVoxImplantUser();
+  // }
 
-  startCall(): void {
-    this.voximplantService.callUser(
-      "6872d2d67c39260acfc71cd9",
-      this.remoteAudioRef
-    );
-  }
+  // startCall(): void {
+  //   this.voximplantService.callUser(
+  //     "6872d2d67c39260acfc71cd9",
+  //     this.remoteAudioRef
+  //   );
+  // }
 
-  endCall(): void {
-    // this.voximplantService.hangup();
-  }
+  // endCall(): void {
+  //   // this.voximplantService.hangup();
+  // }
 
   private setUpSeo() {
     const title = 'Skriin AI TV';
