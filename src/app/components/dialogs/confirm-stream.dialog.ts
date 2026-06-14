4f3b12ebad7fd0589aa36a579dc470239b6437b6
Jackie <jnguye89@gmail.com>
2026-06-14 12:59:25 -0700

    Fix more buttons

--- src/app/components/dialogs/confirm-stream.dialog.ts
+++ src/app/components/dialogs/confirm-stream.dialog.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { GamepadFocusableDirective } from '../../directives/gamepad-focusable.directive';
 
 @Component({
   standalone: true,
@@ -10,11 +11,11 @@ import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/materia
     <h2 mat-dialog-title>{{data.title}}</h2>
     <mat-dialog-content>{{data.body}}</mat-dialog-content>
     <mat-dialog-actions align="end">
-      <button mat-button *ngIf="!!data.cancelBtnText" (click)="close(false)">{{data.cancelBtnText}}</button>
-      <button mat-raised-button color="primary" (click)="close(true)">{{data.confirmBtnText}}</button>
+      <button gamepadFocusable mat-button *ngIf="!!data.cancelBtnText" (click)="close(false)">{{data.cancelBtnText}}</button>
+      <button gamepadFocusable mat-raised-button color="primary" (click)="close(true)">{{data.confirmBtnText}}</button>
     </mat-dialog-actions>
   `,
-  imports: [MatDialogModule, MatButtonModule, CommonModule]
+  imports: [MatDialogModule, MatButtonModule, CommonModule, GamepadFocusableDirective]
 })
 export class ConfirmEndStreamDialog {
   constructor(
